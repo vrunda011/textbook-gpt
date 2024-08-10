7944185f22ebdd6bb3a63cfa45e02b73dd9f7cb3
Ayush54patel <ayushpatel542003@gmail.com>
2024-08-11 01:40:43 +0530

    change frontend

---
+++ frontend/src/components/Nevbar/Navbar.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import "./Navbar.css"
+import nav_logo from "../../assets/Images/Textbook.png"
+
+const Navbar = () => {
+  return (
+    <>
+      <div className='navbar'>
+        <div className='nav-logo'>
+            <img src={nav_logo} alt="" className='nav-logo-img' />
+            <a href='/'>TextBook GPT</a>
+        </div>
+        <div className='nav-links'>
+            <a href='/'>Home</a>
+            <a href='/'>About Us</a>
+            <a href='/'>Contact Us</a>
+        </div>
+        <div className='nav-login-signup'>
+            <div className='login-btn'>Sign in</div>
+        </div>
+      </div>
+      {/* <div className='navbar-bottem'>
+        <p>Are your school student for an online learning?</p>
+        <p className='navbar-bottem-blue'>Talk to us</p>
+      </div> */}
+    </>
+  )
+}
+
+export default Navbar
---
+++ frontend/src/pages/Home/Home.jsx
@@ -0,0 +1,18 @@
+import React from 'react'
+import Hero from '../../components/Hero/Hero'
+import ImageSlider from '../../components/Slider/ImageSlider'
+
+
+
+
+
+const Home = () => {
+  return (
+    <>
+        <Hero />
+        <ImageSlider />
+    </>
+  )
+}
+
+export default Home
