a8d853e89bca81f929cdf0ed6ec59701d6c419d8
Ayush54patel <ayushpatel542003@gmail.com>
2024-08-25 23:33:59 +0530

    changes

--- frontend/src/components/Hero/Hero.jsx
+++ frontend/src/components/Hero/Hero.jsx
@@ -1,39 +1,38 @@
-import React from 'react'
-import "./Hero.css"
-import hero_right_1 from "../../assets/Images/left1.jpeg"
-import hero_right_2 from "../../assets/Images/left2.jpeg"
-import hero_right_3 from "../../assets/Images/left3.jpeg"
-import hero_right_4 from "../../assets/Images/left4.jpeg"
-import { Link } from 'react-router-dom'
+import React from 'react';
+import './Hero.css';
+import hero_right_1 from '../../assets/Images/left1.jpeg';
+import hero_right_2 from '../../assets/Images/left2.jpeg';
+import hero_right_3 from '../../assets/Images/left3.jpeg';
+import hero_right_4 from '../../assets/Images/left4.jpeg';
+import { Link } from 'react-router-dom';
 
 const Hero = () => {
   return (
     <div className='hero-section'>
-        <div className='hero-left'>
-            <p className='hero-left-start'>100% SETISFACTION GUARANTEE</p>
-            <p className='hero-left-h1'>Your doubt </p>
-            <p className='hero-left-h1'>support assistant</p>
-            <div className='hero-left-second'>
-                <p className='hero-left-h3'>Get your doubts solved anytime,anywhere.</p>
-                <p className='hero-left-h3'>completely tailored to your syllabus</p>
-            </div>
-            <div className='hero-left-btn'>
-                <Link to="/login" className='login-link'><div>Get Started</div></Link>
-                {/* <div>See How it works</div> */}
-            </div>
+      <div className='hero-left'>
+        <p className='hero-left-start'>100% SATISFACTION GUARANTEE</p>
+        <p className='hero-left-h1'>Your doubt </p>
+        <p className='hero-left-h1'>support assistant</p>
+        <div className='hero-left-second'>
+          <p className='hero-left-h3'>Get your doubts solved anytime, anywhere.</p>
+          <p className='hero-left-h3'>Completely tailored to your syllabus</p>
         </div>
-        <div className='hero-right'>
-            <div className='hero-right-img'>
-                <img src={hero_right_1} alt="" className='hero-right-img-1'/>
-                <img src={hero_right_2} alt="" className='hero-right-img-2'/>
-                <img src={hero_right_3} alt="" className='hero-right-img-3'/>
-                <img src={hero_right_4} alt="" className='hero-right-img-4'/>
-            </div>
-            <div className='hero-right-bg-1'></div>
-            <div className='hero-right-bg-2'></div>
+        <div className='hero-left-btn'>
+          <Link to="/standard" className='login-link'><div>Get Started</div></Link>
         </div>
+      </div>
+      <div className='hero-right'>
+        <div className='hero-right-img'>
+          <img src={hero_right_1} alt="" className='hero-right-img-1'/>
+          <img src={hero_right_2} alt="" className='hero-right-img-2'/>
+          <img src={hero_right_3} alt="" className='hero-right-img-3'/>
+          <img src={hero_right_4} alt="" className='hero-right-img-4'/>
+        </div>
+        <div className='hero-right-bg-1'></div>
+        <div className='hero-right-bg-2'></div>
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Hero
+export default Hero;
